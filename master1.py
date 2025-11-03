35d9368d4f54da4383c5a23eb265385db4e7a62a
karvail86 <kozonalievcyngyz27@gmail.com>
2025-11-03 10:46:38 +0600

    Update master1.py

--- master1.py
+++ master1.py
@@ -1,1 +1,16 @@
-print('GitHub I know')
+1. pip install django
+2. django-admin startproject ... (mysite)
+3. cd ... (mysite)
+4. python manage.py startapp ... (lalafo_app)
+5. settings.py
+6. pip install python-dotenv
+7. models.py
+8. python manage.py makemigrations
+9. python manage.py migrate
+10. python manage.py runserver
+11. python manage.py createsuperuser
+12. admin.py register
+13. views.py
+14. templates 
+15. urls.py
+16. pip install pillow
